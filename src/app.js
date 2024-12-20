8ac13f52cb4468b8c690526d00a4d21edb771902
Sagnik Majumder <clashofclanssagnik@gmail.com>
2024-12-20 16:47:42 +0530

    Added a public folder

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({origin: process.env.CORS_ORIGIN}))
-app.use(express.json({limit: "20kb"}))
-app.use(express.urlencoded({extended: true, limit: "20kb"}))
+app.use(express.json({limit: "500kb"}))
+app.use(express.urlencoded({extended: true, limit: "500kb"}))
 app.use(cookieParser())
 app.use(express.static("public"))
 
