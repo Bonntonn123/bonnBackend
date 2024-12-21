f9b745ba1c65b04b3b90a6dde9355a8a47456f8a
Sagnik Majumder <clashofclanssagnik@gmail.com>
2024-12-21 10:38:45 +0530

    CORS

--- src/app.js
+++ src/app.js
@@ -4,7 +4,16 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 
-app.use(cors())
+const allowedOrigins = ['https://mithai-upload.netlify.app/', 'http://localhost:5173'];
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }));
 app.use(express.json({limit: "500kb"}))
 app.use(express.urlencoded({extended: true, limit: "500kb"}))
 app.use(cookieParser())
